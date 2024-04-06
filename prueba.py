4346b66637e6f92ef37259d355dcbe9515f013a1
santidemo <abansantiago2.0@gmail.com>
2024-04-06 08:43:57 -0300

    Cuarto commit

--- prueba.py
+++ prueba.py
@@ -20,3 +20,12 @@ print("1_ Suma")
 print("2_ Multiplicacion")
 print("3_ Resta")
 print("4_ Division")
+
+opcion = input("Seleccione un numero para su operacion ")
+n1=float(input("Ingrese primer numero"))
+n2=float(input("Ingrese segundo numero"))
+
+if opcion == "1":
+    resultado = suma(n1,n2)
+    print("El resultado de la suma es:",resultado)
+    
