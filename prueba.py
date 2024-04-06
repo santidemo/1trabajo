bbeca7abf21b5d7bf5dde4d4792fe82e572d4553
santidemo <abansantiago2.0@gmail.com>
2024-04-06 08:40:16 -0300

    tercer commit

--- prueba.py
+++ prueba.py
@@ -4,3 +4,19 @@ def resta(a,b):
     return a -b
 def multiplicacion(a,b):
     return a * b
+def division(a,b):
+    if b != 0:
+        return a / b
+    else:
+        return "No se puede dividir con 0"
+    
+
+#Entorno Visual para el usuario
+
+print("Calculadora")
+print(" ")
+print("Operaciones:")
+print("1_ Suma")
+print("2_ Multiplicacion")
+print("3_ Resta")
+print("4_ Division")
